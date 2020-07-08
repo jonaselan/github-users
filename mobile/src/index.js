3c4964d5095536a09574551081d67358a2661070
Jonas Elan <jonas.elan@gmail.com>
2020-07-07 22:03:23 -0300

    Setup status bar

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,10 +1,16 @@
 
 import React from 'react';
-
+import { StatusBar } from 'react-native'
 import './config/ReactotronConfig';
+
 import Routes from './routes';
 
 const App: () => React$Node = () => {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 };
 export default App;
